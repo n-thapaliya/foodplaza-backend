80f2dbbc457a882a817f0f235606f16991ccb1f8
Nagajan Thapaliya <nagajanthapaliya2004@gmail.com>
2026-06-25 18:59:11 +0530

    Support DATABASE_URL for production

--- config/database.js
+++ config/database.js
@@ -16,6 +16,7 @@ module.exports = {
       idle: 10000,
     },
   },
+
   test: {
     username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
@@ -25,16 +26,16 @@ module.exports = {
     dialect: 'postgres',
     logging: false,
   },
+
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT) || 5432,
+    use_env_variable: 'DATABASE_URL',
     dialect: 'postgres',
     logging: false,
     dialectOptions: {
-      ssl: { require: true, rejectUnauthorized: false },
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
     },
     pool: {
       max: 20,
